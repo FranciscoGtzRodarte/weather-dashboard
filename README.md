f36aa3399e0c6eb0975104ad51f014d97fc055fb
Francisco J Gutierrez Rodarte <Francisco.GutierrezRodarte@du.edu>
2023-02-24 02:55:13 -0800

    upload updated index.html file for weather dashboard

---
+++ README.md
@@ -0,0 +1,50 @@
+# Weather Dashboard : Server-Side APIs Challenge
+
+## Description
+
+A weather dashboard that shows the current weather, temperature, wind speed and humidity percentage of a city. Besides it will also show a forecast of the following 5 days. It runs in the browser and features dynamically updated HTML and CSS. It also uses Third-party APIs that allows to access data and functionality by making requests with specific parameters to a URL.
+
+## User Story
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./assets/img/06-server-side-apis-homework-demo.png)
+
+## Instructions
+
+1. Just input a city name and click the search button.
+2. Then the app will show you its current weather, temperature, wind speed and humidity percentage. And the forecast for the following 5 days.
+3. Once you search a city, it will be saved in the sidebar as button to be able to click again if you prefer.
+
+## APIS Used
+
+It uses the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities and
+and the [Day API]() to retrieve the current date.
+
+## Deployed Site Link
+
+https://franciscogtzrodarte.github.io/work-day-scheduler/
+
+## Thank you for using this app
---
+++ assets/js/script.js
@@ -0,0 +1,210 @@
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// Wraps all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(function () {
+  //variable declarations
+  var APIKey = "8842b0a63cc95a90d2cde5cc0a71cb7a";
+  var initCity = "Denver";
+  var todayEl = $("#today-content");
+  var weekEl = $("#week-content");
+  var cityNameEl = $("#city-name");
+  var citiesEl = $("#cities");
+  var citiesArr = [];
+  var today = dayjs();
+  $("#currentDay").text(today.format("MMM D, YYYY"));
+  var tempEl = $(".temp");
+  var windEl = $(".wind");
+  var humdEl = $(".humidity");
+
+  //checking if there is any storage array yet, if not null creates the sidebar buttons with the data of the array stored at LocalStorage
+  var storedCities = JSON.parse(localStorage.getItem("citySearch"));
+  if (storedCities !== null) {
+    citiesArr = storedCities;
+    //console.log(citiesArr);
+
+    for (var i = 0; i < citiesArr.length; i++) {
+      var cityText = citiesArr[i];
+      var cityBtnEl = document.createElement("button");
+      cityBtnEl.classList.add("btn", "p-3", "btn-block-side", "w-100", "my-2");
+      cityBtnEl.textContent = cityText;
+      citiesEl.append(cityBtnEl);
+    }
+  }
+  displayAPI(initCity); //starts with Denver city as default
+
+  /////////////
+  /////////////////////////////////////////////////////functions
+
+  //displays current Weather from Open Weather API
+  function displayAPI(city) {
+    var queryURL =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      APIKey +
+      "&units=imperial";
+
+    //console.log(queryURL);
+
+    // fetch query
+    fetch(queryURL)
+      .then(function (response) {
+        //console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        // console.log(data);
+        //adds values from data to HTML elements
+        tempEl.text(data.main.temp + " °F");
+        windEl.text(data.wind.speed + " MPH");
+        humdEl.text(data.main.humidity + " %");
+        cityNameEl.text(data.name);
+
+        //gets custom icons
+        var iconKeyword = data.weather[0].main;
+        var iconurl = "./assets/img/" + iconKeyword + ".png";
+        $("#wicon").attr("src", iconurl);
+        //gets icons from Open Weather
+        // var iconcode = data.weather[0].icon;
+        // //console.log(data.weather[0].icon);
+        // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        // $("#wicon").attr("src", iconurl);
+
+        //gets latitude and longitude
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        //console.log("lat=" + lat + "   and lon= " + lon);
+        // var timezone = data.timezone;
+        // console.log("timezone" + timezone);
+
+        weekEl.empty(); //removes children of week container
+        forecast(lat, lon); //calls forecast function with lat and lon as parameters
+      });
+  }
+
+  //function declaration name forecast with two values as arguments
+  function forecast(lat, lon) {
+    //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+    var queryURL =
+      "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      APIKey +
+      "&units=imperial";
+
+    //console.log(queryURL);
+    // fetch query
+    fetch(queryURL)
+      .then(function (response) {
+        //console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+
+        //iterates the data.list array to display 5 days
+        for (var i = 0; i < data.list.length; i = i + 8) {
+          //creates elements
+          var forecastDay = $("<div>");
+          var fdateEl = $("<h3>");
+          var ftempEl = $("<p>");
+          var fwindEl = $("<p>");
+          var fhumdEl = $("<p>");
+
+          //gets icon url and is added to img src
+          // var iconcode = data.list[i].weather[0].icon;
+          // var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+          // var iconEl = $("<img>");
+          // iconEl.attr("src", iconurl);
+
+          //gets custom icons
+          var iconKeyword = data.list[i].weather[0].main;
+          console.log(iconKeyword);
+          var iconurl = "./assets/img/" + iconKeyword + ".png";
+          var iconEl = $("<img>");
+          iconEl.attr("src", iconurl);
+
+          /////////////playing with dates data
+
+          // var fDay = Number(data.list[i].dt_txt.slice(8, 10));
+          // var forecastMonth = Number(data.list[i].dt_txt.slice(5, 7));
+          // var forecastYear = data.list[i].dt_txt.slice(0, 4);
+
+          // console.log(fDay);
+          // console.log(forecastMonth);
+          // console.log(forecastYear);
+          // console.log(data.list[i].dt);
+          // console.log(timezone);
+          var dayname = new Date(data.list[i].dt * 1000).toLocaleDateString(
+            "en",
+            {
+              weekday: "long",
+            }
+          );
+          //console.log(dayname);
+          // console.log(
+          //   "minus" + new Date(data.list[i].dt * 1000 - timezone * 1000)
+          // ); // minus
+          // console.log(
+          //   "plus" + new Date(data.list[i].dt * 1000 + timezone * 1000)
+          // ); // plus
+
+          //adds values from data to HTML elements
+          //fdateEl.text(data.list[i].dt_txt.slice(0, 10));
+          fdateEl.text(dayname);
+          //console.log(fdateEl.text(data.list[i].dt_txt.slice(0, 10)));
+          ftempEl.text("Temp.: " + data.list[i].main.temp + " °F");
+          fwindEl.text("Wind: " + data.list[i].wind.speed + " MPH");
+          fhumdEl.text("Humidity: " + data.list[i].main.humidity + " %");
+
+          //add elements to DOM
+          forecastDay.append(iconEl, fdateEl, ftempEl, fwindEl, fhumdEl);
+          forecastDay.addClass(
+            "col-sm-12 col-md-4 col-lg-2 bg-main forecast-cards text-light text-center rounded-2 mx-2 mb-4"
+          );
+          // console.log(forecastDay);
+          weekEl.append(forecastDay);
+        }
+      });
+    $(".week-content div").addClass("col-md-2");
+  }
+
+  //creates and displays sidebar city buttons
+  function displayBtns() {
+    //iterates the array store in local storage to create city buttons in side bar
+    for (var i = 0; i < citiesArr.length; i++) {
+      var cityText = citiesArr[i];
+      var cityBtnEl = document.createElement("button");
+      cityBtnEl.classList.add("btn", "btn-block-side", "w-100", "my-2", "p-3");
+      cityBtnEl.textContent = cityText;
+    }
+    citiesEl.append(cityBtnEl);
+  }
+
+  ////////////
+  /////////////////////////////////////////////////////EvenListeners
+
+  // search button event listener
+  $(".btn-search").on("click", function (event) {
+    event.preventDefault();
+    var cityName = $(this).siblings("#search-input").val();
+    // if the array dont have the input city value then push value to array and call function displayBtns
+    if (!citiesArr.includes(cityName)) {
+      citiesArr.push($(this).siblings("#search-input").val());
+      displayBtns();
+    }
+    //store array in LocalStorage
+    localStorage.setItem("citySearch", JSON.stringify(citiesArr));
+    displayAPI(cityName); //calls function displayAPI with cityName as parameter
+  });
+
+  //sidebar city buttons eventListeners
+  $("#cities").on("click", ".btn-block-side", function () {
+    var cityName = $(this).text(); //clicked button text = city
+    displayAPI(cityName); //calls function displayAPI with cityName as parameter
+  });
+});
